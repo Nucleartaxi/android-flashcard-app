4a77e654963418e9daa256eed9b9970440e6ca62
LessonsByAlex <alexander@truewheels.net>
2020-03-06 15:01:04 -0800

    Lab 2

---
+++ app/src/main/java/com/example/alexander/flashcard/AddCardActivity.java
@@ -0,0 +1,35 @@
+package com.example.alexander.flashcard;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+
+        findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.save_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("string1", ((EditText) findViewById(R.id.edit_question)).getText().toString());
+                data.putExtra("string2", ((EditText) findViewById(R.id.edit_answer)).getText().toString());
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/alexander/flashcard/MainActivity.java
+++ app/src/main/java/com/example/alexander/flashcard/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.alexander.flashcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
             }
         });
+
+
+
+        findViewById(R.id.add_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (data != null) {
+                String string1 = data.getExtras().getString("string1");
+                String string2 = data.getExtras().getString("string2");
+
+                TextView tv1 = (TextView)findViewById(R.id.flashcard_question);
+                tv1.setText(string1);
+                TextView tv2 = (TextView)findViewById(R.id.flashcard_answer);
+                tv2.setText(string2);
+            }
+        }
     }
 }

a3a51263585266698b44d00ad448bf964e38a350
LessonsByAlex <alexander@truewheels.net>
2020-03-11 23:10:09 -0700

    Lab 3 Finished

--- app/src/main/java/com/example/alexander/flashcard/MainActivity.java
+++ app/src/main/java/com/example/alexander/flashcard/MainActivity.java
@@ -39,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //next button
+        findViewById(R.id.next_right_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // advance our pointer index so we can show the next card
+                    currentCardDisplayedIndex++;
+                    // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                    if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                        currentCardDisplayedIndex = 0;
+                    }
+                    // set the question and answer TextViews with data from the database
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                    //makes answer invisible
+                    findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                }
+                catch(Exception e) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Input a question!");
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText("Input an answer!");
+                }
+
+            }
+        });
+
         //adds card
         findViewById(R.id.add_card_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +74,37 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent, 100);
             }
         });
+
+        //delete button
+        findViewById(R.id.delete_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+
+                //goes back 1 card
+                currentCardDisplayedIndex = currentCardDisplayedIndex - 1;
+                allFlashcards = flashcardDatabase.getAllCards();
+
+                //if there are no flashcards, display empty state
+                if (allFlashcards.size() == 0) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("Input a question!");
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText("Input an answer!");
+                }
+                //if there are flashcards, display them and switch to a new card
+                else {
+                    //automatically switch to a new card (all of this next code is same as next button)
+                    if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                        currentCardDisplayedIndex = 0;
+                    }
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+                    //makes answer invisible
+                    findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
 
         //reads from database
         flashcardDatabase = new FlashcardDatabase(getApplicationContext());
@@ -71,13 +129,15 @@ public class MainActivity extends AppCompatActivity {
                 TextView tv2 = (TextView)findViewById(R.id.flashcard_answer);
                 tv2.setText(answer);
 
-                int i = 0;
-                while (i < allFlashcards.size()) {
-                    String card = allFlashcards.get(i).getQuestion().toString();
-                    Log.d("deleted card", card);
-                    flashcardDatabase.deleteCard(card);
-                    i = i + 1;
-                }
+                //clears database
+                //int i = 0;
+                //while (i < allFlashcards.size()) {
+                //    String card = allFlashcards.get(i).getQuestion().toString();
+                //    Log.d("deleted card", card);
+                //    flashcardDatabase.deleteCard(card);
+                //    i = i + 1;
+                //}
+
                 flashcardDatabase.insertCard(new Flashcard(question, answer));
                 allFlashcards = flashcardDatabase.getAllCards();
 
